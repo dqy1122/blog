b1d71e0d8faf986823f40b60cd3208ea7783ac17
Qingyi Dong <dongqy1020@gmail.com>
2022-07-30 10:17:27 +0800

    第二天

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,50 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.urls import reverse
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    class Meta:
+        verbose_name = '分类'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+class Tag(models.Model):
+    """
+    标签 Tag 也比较简单，和 Category 一样。
+    再次强调一定要继承 models.Model 类！
+    """
+    name = models.CharField(max_length=100)
+    class Meta:
+        verbose_name = '标签'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    """
+    文章的数据库表稍微复杂一点，主要是涉及的字段更多。
+    """
+
+    title = models.CharField('标题', max_length=70)
+    body = models.TextField('正文')
+    created_time = models.DateTimeField('创建时间', default=timezone.now)
+    modified_time = models.DateTimeField('修改时间')
+    excerpt = models.CharField('摘要', max_length=200, blank=True)
+    category = models.ForeignKey(Category, verbose_name='分类', on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, verbose_name='标签', blank=True)
+    author = models.ForeignKey(User, verbose_name='作者', on_delete=models.CASCADE)
+    def save(self, *args, **kwargs):
+        self.modified_time = timezone.now()
+        super().save(*args, **kwargs)
+    class Meta:
+        verbose_name = '文章'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.title
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
